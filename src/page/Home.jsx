d489558a610cb903a8ef45d82fa7feaf162b4557
ahevr <ahmethusreversen@hotmail.com>
2023-03-09 15:02:30 +0300

    setTo

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
                 <div className="flex flex-col items-center justify-center mt-28 text-center space-y-5">
                     <img src={Logo} alt="" width={150} />
                     <h1 className='text-3xl font-semibold text-white'>React Hava Durumu Uygulaması</h1>
-                    <p className='text-sm text-gray-400 w-1/2 '>React ile yapılmış bu hava durumu uygulaması react-router-dom,tailwindcss ve openweathermap apisi kullanarak yapılmıştır...</p>
+                    <p className='text-sm text-gray-400 lg:w-1/2 '>React ile yapılmış bu hava durumu uygulaması react-router-dom,tailwindcss ve openweathermap apisi kullanarak yapılmıştır...</p>
                     <Link to="weathers"
                         className='px-16 py-4 bg-blue-500 rounded-2xl text-white text-sm flex items-center hover:bg-blue-800 duration-500'>
                         Hava Durumuna Git
